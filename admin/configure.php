c3a3780226610858ac7b30cee3e997f244ffd336
wiblingen <105824369+wiblingen@users.noreply.github.com>
2026-01-25 13:21:52 +0100

    Update dashboard link text to Czech translation

--- admin/configure.php
+++ admin/configure.php
@@ -863,9 +863,8 @@ if (file_exists($bmAPIkeyFile) && fopen($bmAPIkeyFile,'r')) {
             </div>
 			<a class="noMob menureset" href="javascript:factoryReset();"><?php echo __( 'Factory Reset' );?></a>
 			<a class="noMob menubackup" href="/admin/config_backup.php"><?php echo __( 'Backup/Restore' );?></a>
-			<a class="menuupdatedashboard" href="/update-dashboard/update-dashboard.php">'.__( 'Update-dashboard' ).'</a>';
+			<a class="menuupdatedashboard" href="/update-dashboard/update-dashboard.php"><?php echo __( 'Aktualizace Dashboardu' );?></a>
 			<a class="noMob menuupdate" href="/admin/advanced/services.php?action=updatehostsfiles"><?php echo __( 'ODS Update' );?></a>
-            <a class="noMob menuupdate" href="/admin/advanced/services.php?action=updatehostsfiles"><?php echo __( 'hovno' );?></a>
 			<a class="noMob menuadvanced" href="/admin/advanced/">Advanced</a>
 			<a class="menupower" href="/admin/power.php"><?php echo __( 'Power' );?></a>
 			<a class="menuadmin" href="/admin/"><?php echo __( 'Admin' );?></a>
